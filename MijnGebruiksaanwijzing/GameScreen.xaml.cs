4c322bd2bb301e4bb232911d17fa9947c49c2247
Bas Lemmen <PS197800@summacollege.nl>
2020-05-27 15:42:50 +0200

    Conncetion with database

--- MijnGebruiksaanwijzing/GameScreen.xaml.cs
+++ MijnGebruiksaanwijzing/GameScreen.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MijnGebruiksaanwijzing.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace MijnGebruiksaanwijzing
         public GameScreen(string categorie, string mentorEmail, string studentEmail)
         {
             InitializeComponent();
+            DBConnection conn = new DBConnection();
+
+            conn.GetCards(categorie, "geel");
+            conn.GetCards(categorie, "blauw");
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
